ab0bb7075608eabfe036210aa4cc92beab98e29e
Lukasz <contact@suramar.pl>
2025-11-30 20:59:45 +0200

    Fix account creation for ALB Controller

--- modules/alb-ingress-controller/main.tf
+++ modules/alb-ingress-controller/main.tf
@@ -13,7 +13,7 @@ terraform {
   }
 }
 
-# 3) Create ServiceAccount
+# 3) Create ServiceAccount role
 module "load_balancer_controller_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
   version = "6.2.3"
@@ -30,15 +30,33 @@ module "load_balancer_controller_irsa" {
   }
 }
 
-# 4) Install Helm Chart
+# 4) Create ServiceAccount role
+resource "kubernetes_service_account" "lbc_sa" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = var.namespace
+
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.load_balancer_controller_irsa.arn
+    }
+  }
+}
+
+# 5) Install Helm Chart
 resource "helm_release" "aws_load_balancer_controller" {
   name = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   namespace  = var.namespace
   depends_on = [
-    module.load_balancer_controller_irsa
+    kubernetes_service_account.lbc_sa
   ]
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
   set {
     name  = "replicaCount"
     value = 1
